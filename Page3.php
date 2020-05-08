35553ef1c34eeacc514e917bb69e310b1ea6a439
Morgane1509 <fany-morgane.minto@ism.edu.sn>
2020-05-08 05:24:02 +0000

    Page de Connexion

---
+++ Page3.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>PageConnexion</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link type="text/css" rel="stylesheet" href="style.css">
+
+</head>
+  <body>
+
+  <header>
+		<div class="logo">
+		
+		
+		</div>
+		
+		<h2>
+			Le plaisir de jouer
+		
+		</h2>
+	
+	
+	</header>
+	
+	<section class="transpa">
+	
+	</section>
+	
+	<section class="mainContainer">
+		<section class="formulaire">
+		
+			<h2>
+				Login Form
+			
+			</h2>
+			
+			<div>
+				
+				<form>
+					
+					<p>
+						<input class="champs" type="text" name="login" placeholder="Login">
+						
+						<input class="champs" type="text" name="mdp" placeholder="Password">					
+                    </p>
+                    
+                    <div class="bof">
+                        <p class="fond">
+                            <button type="text" name="submit-btn" placeholder="Password">Connexion</button>
+                        </p>
+                        
+                        <p>
+                            <label><a href="#">S'incrire pour jouer</a></label>
+                        </p>				
+                    </div>
+				</form>
+			
+			</div>
+		
+		
+		</section>
+
+
+	</section>
+
+      
+    
+  
+  
+  
+  
+  
+  
+  
+  
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
